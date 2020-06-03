b39268e211f8c9f43940a5b51d717b543efb1eed
saurabhh404 <saurabhh404@gmail.com>
2020-06-04 00:14:24 +0530

    Final

--- app.js
+++ app.js
@@ -1,15 +1,48 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + '/date.js');
 const app = express();
 
-app.get("/", function(req, res){
-  var currentDay = new Date().getDay();
-  if(currentDay === 6 || currentDay === 0){
-    res.send("Yay! It's weekend!");
-  } else{
-    res.send("Nope, you HAVE to work today.")
-  }
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+const items = [];
+const workItems = [];
+
+app.get("/", function(req, res) {
+  const day = date.getDate();
+  res.render("list", {
+    listTitle: day,
+    newItems: items
+  });
 });
 
+app.post("/", function(req, res) {
+  const item = req.body.nextItem;
+  if (req.body.submit === "Work List") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 
-app.listen(3000, console.log("Server running at https://localhost:3000/"))
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newItems: workItems
+  });
+});
+
+app.post("/work", function(req, res) {});
+
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
+app.listen(3000, console.log("Server running at http://localhost:3000/"))
